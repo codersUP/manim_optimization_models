7105db4cc50ada166676fcbca52af23226964ffb
kikexd <enrique06660@gmail.com>
2022-02-15 17:12:50 -0500

    tree structure formed

--- main.py
+++ main.py
@@ -73,16 +73,30 @@ def f_func(vars, func):
 
 def branch_and_bound_int(vars, func, constraints, initial_point):
     vars = [sp.symbols(i) for i in vars]
-    constraints = get_constraints(constraints, vars)
+    constraints_m = get_constraints(constraints, vars)
 
     min = MIN
     min_values = initial_point.copy()
+    step = 0
 
-    stack = [{"initial_point": initial_point.copy(), "constraints": constraints.copy()}]
+    root = {
+        "father": None,
+        "added_constraints": [
+            move_inequality_constants(parse_expr(c)) for c in constraints
+        ],
+        "childrens": [],
+        "lv": 0,
+        "step": step,
+        "initial_point": initial_point.copy(),
+        "constraints": constraints_m.copy(),
+    }
+
+    stack = [root]
 
     while len(stack):
 
         act = stack.pop()
+        lv = act["lv"]
         values = act["initial_point"]
         cons = act["constraints"]
 
@@ -95,42 +109,62 @@ def branch_and_bound_int(vars, func, constraints, initial_point):
                 if not check_int(v, EPSILON):
                     all_int = False
 
-                    stack.append(
-                        {
-                            "initial_point": m.x.copy(),
-                            "constraints": cons.copy()
-                            + [
-                                {
-                                    "type": "ineq",
-                                    "fun": c_func(vars, int(v) - vars[i]),
-                                }
-                            ],
-                        }
-                    )
-                    stack.append(
-                        {
-                            "initial_point": m.x.copy(),
-                            "constraints": cons.copy()
-                            + [
-                                {
-                                    "type": "ineq",
-                                    "fun": c_func(vars, vars[i] - (int(v) + 1)),
-                                }
-                            ],
-                        }
-                    )
+                    left_children = {
+                        "father": act,
+                        "added_constraints": sp.LessThan(vars[i], int(v)),
+                        "childrens": [],
+                        "lv": lv + 1,
+                        "step": step + 1,
+                        "initial_point": m.x.copy(),
+                        "constraints": cons.copy()
+                        + [
+                            {
+                                "type": "ineq",
+                                "fun": c_func(vars, int(v) - vars[i]),
+                            }
+                        ],
+                    }
+                    stack.append(left_children)
+                    step += 1
+                    act["childrens"].append(left_children)
+
+                    right_children = {
+                        "father": act,
+                        "added_constraints": sp.GreaterThan(vars[i], int(v) + 1),
+                        "childrens": [],
+                        "lv": lv + 1,
+                        "step": step + 1,
+                        "initial_point": m.x.copy(),
+                        "constraints": cons.copy()
+                        + [
+                            {
+                                "type": "ineq",
+                                "fun": c_func(vars, vars[i] - (int(v) + 1)),
+                            }
+                        ],
+                    }
+                    stack.append(right_children)
+                    step += 1
+                    act["childrens"].append(right_children)
+
             if all_int:
                 min = m.fun
                 min_values = m.x
 
-    print(min, min_values)
+    return {"min": min, "min_values": min_values, "tree": root}
 
 
 def main():
     f = open("bab.json")
     data = json.load(f)
 
-    branch_and_bound_int(**data)
+    m = branch_and_bound_int(**data)
+    min = m["min"]
+    min_values = m["min_values"]
+    tree = m["tree"]
+
+    print(min, min_values)
+    pprint(tree)
 
 
 if __name__ == "__main__":
