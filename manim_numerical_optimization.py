58b2c6158f985fca86355c9cb1b6055175c8b780
kikexd <enrique06660@gmail.com>
2022-02-18 02:34:23 -0500

    add numerical optimization 2d

--- manim_numerical_optimization.py
+++ manim_numerical_optimization.py
@@ -144,3 +144,128 @@ class NO3D(ThreeDScene):
         self.stop_ambient_camera_rotation()
 
         self.wait()
+
+
+class NO2D(Scene):
+    def add_method_text(self, text):
+        method_text = Text(
+            text,
+            font_size=20,
+        )
+        self.add(method_text)
+
+        self.play(Write(method_text.to_corner(UL)))
+
+        return method_text
+
+    def add_text_points_gradient(
+        self, points, axes, text_func, method_text, get_coordintes, delete_points=True
+    ):
+        points2d = VGroup()
+
+        for i, point in enumerate(points):
+            text_act = Text(
+                text_func(point),
+                font_size=10,
+            )
+
+            point2d = Dot(
+                point=axes.coords_to_point(*get_coordintes(point)),
+                color=RED,
+            )
+
+            points2d += point2d
+
+            self.add(text_act)
+
+            self.play(
+                FadeIn(point2d),
+                Write(text_act.next_to(method_text, DOWN).align_on_border(LEFT)),
+            )
+            self.play(point2d.animate.set_color(GRAY), FadeOut(text_act))
+            self.wait(1)
+
+        if delete_points:
+            self.play(FadeOut(points2d))
+
+    def construct(self):
+        f = open("numerical_optimization.json")
+        data = json.load(f)
+
+        func = data["func"]
+        vars = data["vars"]
+        u_range = data["u_range"]
+        v_range = data["v_range"]
+
+        if len(vars) != 1:
+            raise Exception("the model must have 1 variable1")
+
+        func = sp.parse_expr(func)
+        func_lambda = sp.Lambda(vars, func)
+        func_evaluated = lambda x: np.array([func_lambda((x))], dtype=float)
+
+        g = gradient.gradient(**data)
+        gc = gradient_conj.gradient_conj(**data)
+        n = newton.newton(**data)
+
+        axes = Axes(
+            x_range=[u_range[0], u_range[1], 1],
+            y_range=[v_range[0], v_range[1], 1],
+            x_length=abs(u_range[1] - u_range[0]),
+            axis_config={"color": GREEN},
+        )
+        axes_labels = axes.get_axis_labels()
+
+        function = axes.plot(lambda x: func_evaluated(x), color=BLUE)
+
+        axes_function = VGroup(axes, axes_labels, function)
+
+        self.play(FadeIn(axes_function))
+
+        method_text = self.add_method_text("Método de gradiente (Máximo Descenso)")
+
+        self.add_text_points_gradient(
+            g["points"],
+            axes,
+            lambda p: f"point: {p['point']}\nvalue: {p['value']}\ngradient: {p['gradient']}\nalpha: {p['step_arrived']}",
+            method_text,
+            get_coordintes=lambda point: [
+                point["point"][0],
+                point["value"],
+            ],
+        )
+
+        self.play(FadeOut(method_text))
+
+        method_text = self.add_method_text("Método de gradiente conjugado")
+
+        self.add_text_points_gradient(
+            gc["points"],
+            axes,
+            lambda p: f"point: {p['point']}\nvalue: {p['value']}\ns: {p['s']}\nalpha: {p['step_arrived']}",
+            method_text,
+            get_coordintes=lambda point: [
+                point["point"][0],
+                point["value"],
+            ],
+        )
+
+        self.play(FadeOut(method_text))
+
+        method_text = self.add_method_text("Método de Newton")
+
+        self.add_text_points_gradient(
+            n["points"],
+            axes,
+            lambda p: f"point: {p}\nvalue: {func_evaluated(*p)}",
+            method_text,
+            get_coordintes=lambda point: [
+                point[0],
+                func_evaluated(*point),
+            ],
+            delete_points=False,
+        )
+
+        self.play(FadeOut(method_text))
+
+        self.wait()
