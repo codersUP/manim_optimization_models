62655bad848a4b72f0271fbf4e0b375c04fa9453
kikexd <enrique06660@gmail.com>
2022-02-17 23:59:25 -0500

    finish manim bab 3d

--- manim_bab.py
+++ manim_bab.py
@@ -4,7 +4,7 @@ import json
 import numpy as np
 
 
-class BABPlot(ThreeDScene):
+class BAB3D(ThreeDScene):
     def add_text_tree(self, root, axes, father=None):
         text_act = Text(
             f"constraints:\n{root['added_constraints']}\nbest point: {root['best_point']}\nevaluation: {root['evaluation']}",
@@ -17,67 +17,74 @@ class BABPlot(ThreeDScene):
             ),
             color=BLUE,
         )
-        axes.add(point)
+
         self.add(text_act)
 
         if not father:
             self.add_fixed_in_frame_mobjects(text_act)
-            self.add(text_act.to_corner(UL))
-            self.wait()
+            self.play(FadeIn(point), Write(text_act.to_corner(UL)))
 
         else:
             self.add_fixed_in_frame_mobjects(text_act)
-            self.add(text_act.next_to(father, DOWN).align_on_border(LEFT))
-            self.wait()
+            self.play(
+                FadeIn(point),
+                Write(text_act.next_to(father, DOWN).align_on_border(LEFT)),
+            )
+
+        self.play(point.animate.set_color(GREEN))
+        self.wait(1)
 
         for i in root["childrens"]:
             self.add_text_tree(i, axes, father=text_act)
 
-        # self.play(point.animate.set_color(GREEN))
-        self.remove(text_act)
+        self.play(FadeOut(text_act))
 
     def construct(self):
-
         f = open("bab.json")
         data = json.load(f)
 
         func = data["func"]
         vars = data["vars"]
+        u_range = data["u_range"]
+        v_range = data["v_range"]
+        stroke_width = data["stroke_width"]
 
-        if len(vars) == 1:
-            pass
+        if len(vars) != 2:
+            raise Exception("the model must have 2 variables")
 
-        elif len(vars) == 2:
-            m = bab.branch_and_bound_int(**data)
+        m = bab.branch_and_bound_int(**data)
+        axes = ThreeDAxes()
+        function = Surface(
+            lambda u, v: np.array(
+                [
+                    u,
+                    v,
+                    bab.func_eval(
+                        vars,
+                        [u, v],
+                        bab.parse_expr(func),
+                    ),
+                ]
+            ),
+            u_range=u_range,
+            v_range=v_range,
+            checkerboard_colors=[RED_D, RED_E],
+            resolution=(50, 50),
+            fill_opacity=0.5,
+            stroke_width=stroke_width,
+        )
+        self.renderer.camera.light_source.move_to(
+            3 * IN
+        )  # changes the source of the light
+        self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
+        axes_function = VGroup(axes, function)
 
-            axes = ThreeDAxes()
-            function = Surface(
-                lambda u, v: np.array(
-                    [
-                        u,
-                        v,
-                        bab.func_eval(
-                            vars,
-                            [u, v],
-                            bab.parse_expr(func),
-                        ),
-                    ]
-                ),
-                v_range=[-5, 5],
-                u_range=[-5, 5],
-                checkerboard_colors=[RED_D, RED_E],
-                resolution=(50, 50),
-            )
-            self.renderer.camera.light_source.move_to(
-                3 * IN
-            )  # changes the source of the light
-            self.set_camera_orientation(phi=75 * DEGREES, theta=30 * DEGREES)
+        self.play(FadeIn(axes_function))
 
-            axes_function = VGroup(axes, function)
+        self.add_text_tree(m["tree"], axes)
 
-            self.play(FadeIn(axes_function))
-            self.play(axes_function.animate.move_to(np.array([-10, 1, -2])))
+        self.begin_ambient_camera_rotation(rate=0.1)
+        self.wait(50)
+        self.stop_ambient_camera_rotation()
 
-            self.add_text_tree(m["tree"], axes)
-
-            self.wait()
+        self.wait()
