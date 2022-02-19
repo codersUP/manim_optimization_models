4ddf4eef164d4f62ca19ac3aabad5c79be773a9c
Yumenio <alejandroandy447@gmail.com>
2022-02-19 10:41:29 -0500

    line_search integration with streamlit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.simplex.manim_model import Simplex2D as X, Simplex3D as Y
+from src.line_search.line_search import ThreeDCanvas as lineSearch
 
 class TwoDSimplex(X): pass
 
 class ThreeDSimplex(Y): pass
 
+class LineSearch(lineSearch): pass
